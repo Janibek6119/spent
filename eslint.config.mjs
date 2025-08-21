1d1a7633c33077fd9bf34d9a3de4c3c927117980
Janibek6119 <eldar.malyshov@gmail.com>
2025-08-21 20:33:10 +0000

    prettier-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,8 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
-export default withNuxt();
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default withNuxt(eslintPluginPrettierRecommended, {
+  rules: {
+    "prettier/prettier": "warn",
+  },
+});
