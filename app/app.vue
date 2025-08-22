b6338b3d784fc75bcfb71b923a8d45d395391c00
Janibek6119 <eldar.malyshov@gmail.com>
2025-08-22 15:36:00 +0000

    primevue-tailwindcss

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="flex min-h-screen flex-col">
     <BigHello class="flex-center" />
+    <PButton
+      class="mx-auto mt-8 w-fit"
+      severity="info"
+      :label="`Click me x${count}`"
+      :disabled="loading"
+      @click="count++"
+    />
+    <PToast />
+    <PButton
+      class="mx-auto mt-8 w-fit"
+      severity="success"
+      label="Submit"
+      :disabled="loading || !count"
+      :loading="loading"
+      @click="onSubmitFake"
+    />
   </div>
 </template>
+
+<script setup lang="ts">
+const count = ref(0);
+const loading = ref(false);
+const toast = useToast();
+
+const onSubmitFake = () => {
+  loading.value = true;
+  const currentCount = count.value;
+  setTimeout(() => {
+    loading.value = false;
+    toast.add({
+      severity: "success",
+      summary: "Success",
+      detail: `Data submitted successfully: ${currentCount}`,
+      life: 3000,
+    });
+    count.value = 0;
+  }, 1000);
+};
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,28 @@
+import Aura from "@primeuix/themes/aura";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@primevue/nuxt-module"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   vite: { plugins: [tailwindcss()] },
   css: ["~~/app/assets/css/main.css"],
+  primevue: {
+    components: {
+      prefix: "P",
+    },
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: ".dark",
+          cssLayer: {
+            name: "primevue",
+            order: "theme, base, primevue", // not like in TW v3 ("tailwind-base, primevue, tailwind-utilities")
+          },
+        },
+      },
+    },
+  },
 });
